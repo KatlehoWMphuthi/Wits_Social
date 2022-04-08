d8761ac70fc285de317905ba5e92ca0030d09586
KatlehoWMphuthi <101892198+KatlehoWMphuthi@users.noreply.github.com>
2022-04-08 15:33:28 +0200

    Add files via upload

---
+++ app/src/test/java/com/example/witssocial/ValidatorTest.java
@@ -0,0 +1,21 @@
+package com.example.wits_social;
+
+import org.junit.Test;
+import org.junit.internal.runners.JUnit38ClassRunner;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static com.google.common.truth.Truth.assertThat;
+
+
+@RunWith(JUnit4.class)
+
+public class ValidatorTest {
+
+    @Test
+    public void isinputValid(){
+        String a = "Katleho";
+        Boolean b = new Validator(a).isinputString();
+        assertThat(b).isEqualTo(true);
+    }
+}
