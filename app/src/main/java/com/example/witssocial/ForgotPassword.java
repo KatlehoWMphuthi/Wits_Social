1b053810d81a8ac5ed500940a8531d26aa6ea32f
vance97 <vance97@users.noreply.github.com>
2022-04-09 01:21:46 +0200

    Primary Color Update

--- app/src/main/java/com/example/witssocial/ForgotPassword.java
+++ app/src/main/java/com/example/witssocial/ForgotPassword.java
@@ -2,7 +2,9 @@ package com.example.witssocial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+    }
+
+    public void checkMail(View view) {
+        Intent intent = new Intent(this, CheckMail.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/witssocial/Register.java
+++ app/src/main/java/com/example/witssocial/Register.java
@@ -1,14 +1,25 @@
 package com.example.witssocial;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Register extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+    }
+
+    public void loginPage(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public void registerOTP(View view) {
+        Intent intent = new Intent(this, RegisterOTP.class);
+        startActivity(intent);
     }
 }
