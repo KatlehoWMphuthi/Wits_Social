df8b23104b68477d10f8b030c27512547dba3645
vance97 <vance97@users.noreply.github.com>
2022-04-10 02:49:32 +0200

    CheckMail.java Update
    Users can now "Open in Email App" to check whether or not they've received a password reset link on their email inbox.

--- app/src/main/java/com/example/witssocial/CheckMail.java
+++ app/src/main/java/com/example/witssocial/CheckMail.java
@@ -2,7 +2,9 @@ package com.example.witssocial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CheckMail extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class CheckMail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_mail);
+    }
+
+    public void openMailApp(View view) {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_APP_EMAIL);
+        startActivity(intent);
     }
 }
