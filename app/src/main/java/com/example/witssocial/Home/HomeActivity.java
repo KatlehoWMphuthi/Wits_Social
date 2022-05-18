7f89ba22b27f080f9ca2ce30d0524c23724b428a
Moe <2332271@students.wits.ac.za>
2022-05-18 21:19:29 +0200

    Commit to allow for pulling Louis's code to work on activity_profile.xml

--- app/src/main/java/com/example/witssocial/Home/HomeActivity.java
+++ app/src/main/java/com/example/witssocial/Home/HomeActivity.java
@@ -1,35 +1,33 @@
-package com.example.witssocial;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
+package com.example.witssocial.Home;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.witssocial.CreatePost.PostActivity;
+import com.example.witssocial.More.MoreOptionsActivity;
+import com.example.witssocial.Notification.NotificationFragment;
+import com.example.witssocial.R;
+import com.example.witssocial.Search.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.example.witssocial.HomeFragment;
-import com.example.witssocial.NotificationFragment;
-import com.example.witssocial.ProfileFragment;
-import com.example.witssocial.SearchFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
-    private Toolbar topBar;
     private Fragment selectorFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-        topBar = findViewById(R.id.topVar_homeActivity);
-
-
 
         bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
             @Override
@@ -45,15 +43,23 @@ public class HomeActivity extends AppCompatActivity {
                         break;
 
                     case R.id.nav_add :
-                       selectorFragment = new PostFragment();
+                        selectorFragment = null;
+                        startActivity(new Intent(HomeActivity.this , PostActivity.class));
                         break;
 
-                    case R.id.nav_heart :
+                    /*case R.id.nav_add :
+                        selectorFragment = null;
+                        startActivity(new Intent(MainActivity.this , PostActivity.class));
+                        break;*/
+
+                    case R.id.nav_notifications:
                         selectorFragment = new NotificationFragment();
                         break;
 
-                    case R.id.nav_profile :
-                        selectorFragment = new ProfileFragment();
+                    case R.id.nav_more:
+                        //selectorFragment = new ProfileFragment();
+                        selectorFragment = null;
+                        startActivity(new Intent(HomeActivity.this , MoreOptionsActivity.class));
                         break;
                 }
 
@@ -66,17 +72,16 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        /*Bundle intent = getIntent().getExtras();
+        Bundle intent = getIntent().getExtras();
         if (intent != null) {
             String profileId = intent.getString("publisherId");
 
             getSharedPreferences("PROFILE", MODE_PRIVATE).edit().putString("profileId", profileId).apply();
 
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
-            bottomNavigationView.setSelectedItemId(R.id.nav_profile);
+           // getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+            bottomNavigationView.setSelectedItemId(R.id.nav_more);
         } else {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , new HomeFragment()).commit();
-        }*/
+        }
     }
-
 }
