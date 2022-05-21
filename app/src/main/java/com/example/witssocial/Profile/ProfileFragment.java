93429c9908465002a4122b0cd9591967275b680a
Kgotso <Kgotsokg12@gmail.com>
2022-05-21 04:49:25 +0200

    Passed username from a post to profile Fragment and from Search

--- app/src/main/java/com/example/witssocial/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/witssocial/Profile/ProfileFragment.java
@@ -5,15 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.ImageView;
-import android.widget.TextView;
-
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.witssocial.Home.HomeFragment;
 import com.example.witssocial.R;
+import com.example.witssocial.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
+    private FragmentProfileBinding viewBinding;
+    private String username;
     /*TextView profilename,biography,fullName;
     ImageView profilepic,picture;
 */
@@ -23,7 +26,13 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        viewBinding = FragmentProfileBinding.inflate(inflater, container, false);
+        View view = viewBinding.getRoot();
+
+        //Collect Data from Parent activity
+        Bundle bundle = this.getArguments();
+        username = bundle.getString("username");
+
 
 /*
         profilepic = view.findViewById(R.id.image_profile);
@@ -36,5 +45,26 @@ public class ProfileFragment extends Fragment {
 
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+      //  Toolbar toolbar = view.findViewById(R.id.view_profile_toolbar);
+
+        //bind views and set back navigation icon
+        viewBinding.viewProfileToolbar.setNavigationIcon(R.drawable.ic_back);
+        viewBinding.viewProfileToolbar.setTitle(username);
+        viewBinding.viewProfileToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to home fragment
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, new HomeFragment());
+                transaction.commit();
+            }
+        });
+
     }
 }
