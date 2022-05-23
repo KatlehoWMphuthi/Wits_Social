404283e419946e6044ea36bcf9c6142445106867
Katleho <1355854@students.wits.ac.za>
2022-05-23 12:08:00 +0200

    Testing

---
+++ app/src/androidTest/java/com/example/witssocial/Authentication/SignInActivityTest.java
@@ -0,0 +1,30 @@
+package com.example.witssocial.Authentication;
+
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class SignInActivityTest {
+    @Rule
+    public ActivityScenarioRule<SignInActivity> signInActivityActivityScenarioRule =
+            new ActivityScenarioRule<SignInActivity>(SignInActivity.class);
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void goToHomeActivity() {
+    }
+
+    @Test
+    public void updateUI() {
+    }
+
+    @Test
+    public void registerPage() {
+    }
+}
