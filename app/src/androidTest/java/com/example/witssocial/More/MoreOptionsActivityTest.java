4ef8cc530c2e4d108323f24eb151291006e369b3
Katleho <1355854@students.wits.ac.za>
2022-05-23 13:46:09 +0200

    Testing

---
+++ app/src/androidTest/java/com/example/witssocial/More/MoreOptionsActivityTest.java
@@ -0,0 +1,18 @@
+package com.example.witssocial.More;
+
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.example.witssocial.Profile.ProfileActivity;
+
+import org.junit.Test;
+
+public class MoreOptionsActivityTest {
+    ActivityScenarioRule<MoreOptionsActivity> moreOptionsActivityActivityScenarioRule=
+            new ActivityScenarioRule<MoreOptionsActivity>(MoreOptionsActivity.class);
+
+    @Test
+    public void onCreate() {
+    }
+}
