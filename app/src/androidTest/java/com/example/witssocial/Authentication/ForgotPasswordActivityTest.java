b1cc9e95f2b6a6433ad960399cd0a2fa0145938e
Katleho <1355854@students.wits.ac.za>
2022-05-23 11:59:54 +0200

    Testing

---
+++ app/src/androidTest/java/com/example/witssocial/Authentication/ForgotPasswordActivityTest.java
@@ -0,0 +1,17 @@
+package com.example.witssocial.Authentication;
+
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ForgotPasswordActivityTest {
+    @Rule
+    public ActivityScenarioRule<ForgotPasswordActivity> forgotPasswordActivityActivityScenarioRule=
+            new ActivityScenarioRule<ForgotPasswordActivity>(ForgotPasswordActivity.class);
+    @Test
+    public void onCreate() {
+    }
+}
