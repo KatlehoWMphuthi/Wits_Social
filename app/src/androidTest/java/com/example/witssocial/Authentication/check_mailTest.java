67de62479a03d5e52fc17079481227577b12b1a5
Katleho <1355854@students.wits.ac.za>
2022-05-23 11:42:57 +0200

    Testing

---
+++ app/src/androidTest/java/com/example/witssocial/Authentication/check_mailTest.java
@@ -0,0 +1,26 @@
+package com.example.witssocial.Authentication;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.example.witssocial.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class check_mailTest {
+    @Rule
+    public ActivityScenarioRule<check_mail> check_mailActivityScenarioRule =
+            new ActivityScenarioRule<check_mail>(check_mail.class);
+    @Test
+    public void onCreate() {
+        onView(withId(R.id.CheckMailApp)).perform(click());
+    }
+}
