e9a03caedb2833bd3b3dc6dd0ca80d76d0cb7d9c
Katleho <1355854@students.wits.ac.za>
2022-05-28 22:49:02 +0200

    Testing HomeActivity

--- app/src/androidTest/java/com/example/witssocial/Home/HomeActivityTest.java
+++ app/src/androidTest/java/com/example/witssocial/Home/HomeActivityTest.java
@@ -1,19 +1,30 @@
 package com.example.witssocial.Home;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
+import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.witssocial.Authentication.check_mail;
+import com.example.witssocial.R;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(AndroidJUnit4.class)
 public class HomeActivityTest {
     @Rule
     public ActivityScenarioRule<HomeActivity> homeActivityActivityScenarioRule =
             new ActivityScenarioRule<HomeActivity>(HomeActivity.class);
-    /*@Test
-    public void onCreate() {
-    }*/
+
+    @Test
+    public void test_IsNavigationDisplayed() {
+        onView(withId(R.id.bottom_navigation)).check(ViewAssertions.matches(isDisplayed()));
+    }
 }
