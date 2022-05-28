371237e4aedf03de05b41b418a894e27e3796f44
vance97 <vansmuchongo@gmail.com>
2022-05-28 16:29:56 +0000

    Completed WelcomeActivity DarkMode

--- app/src/main/java/com/example/witssocial/WelcomeActivity.java
+++ app/src/main/java/com/example/witssocial/WelcomeActivity.java
@@ -6,6 +6,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -13,15 +15,21 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.witssocial.Authentication.SignInActivity;
 import com.example.witssocial.Authentication.SignUpActivity;
+import com.google.android.material.button.MaterialButton;
 
 public class WelcomeActivity extends AppCompatActivity {
-//ConstraintLayout Background = (ConstraintLayout)findViewById(R.id.background);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        //Fetches elements from XML
         ConstraintLayout Background = (ConstraintLayout)findViewById(R.id.background);
+        ImageView Logo = findViewById(R.id.imageView);
+        MaterialButton WelcomeSignIn = findViewById(R.id.sign_in_text_button);
+        MaterialButton WelcomeSignUp = findViewById(R.id.sign_up_text_button);
+
 
         //Control what happens when DarkMode is turned on
         int nightModeFlags =
@@ -29,7 +37,12 @@ public class WelcomeActivity extends AppCompatActivity {
                         Configuration.UI_MODE_NIGHT_MASK;
         switch (nightModeFlags) {
             case Configuration.UI_MODE_NIGHT_YES:
-                Background.setBackgroundColor(Color.GREEN);
+                Background.setBackgroundColor(Color.BLACK);
+                Logo.setImageResource(R.drawable.appicon_t);
+                WelcomeSignIn.setBackgroundColor(Color.WHITE);
+                WelcomeSignIn.setTextColor(Color.BLACK);
+                WelcomeSignUp.setTextColor(Color.WHITE);
+                WelcomeSignUp.setStrokeColorResource(R.color.white);
                 break;
 
             case Configuration.UI_MODE_NIGHT_NO:
