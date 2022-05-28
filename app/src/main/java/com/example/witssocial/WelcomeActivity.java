92342d2659fc815d9cf93c01e315fa0d87998056
vance97 <vansmuchongo@gmail.com>
2022-05-28 03:33:32 +0200

    Started DarkMode for WelcomeActivity.java

--- app/src/main/java/com/example/witssocial/WelcomeActivity.java
+++ app/src/main/java/com/example/witssocial/WelcomeActivity.java
@@ -1,21 +1,45 @@
 package com.example.witssocial;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.witssocial.Authentication.SignInActivity;
 import com.example.witssocial.Authentication.SignUpActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
+//ConstraintLayout Background = (ConstraintLayout)findViewById(R.id.background);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ConstraintLayout Background = (ConstraintLayout)findViewById(R.id.background);
+
+        //Control what happens when DarkMode is turned on
+        int nightModeFlags =
+                this.getResources().getConfiguration().uiMode &
+                        Configuration.UI_MODE_NIGHT_MASK;
+        switch (nightModeFlags) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                Background.setBackgroundColor(Color.GREEN);
+                break;
+
+            case Configuration.UI_MODE_NIGHT_NO:
+                Background.setBackgroundColor(Color.YELLOW);
+                break;
+        }
+
+
     }
+
     public void goToSignUpActivity(View view) {
 
         startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
@@ -24,4 +48,5 @@ public class WelcomeActivity extends AppCompatActivity {
     public void goToSignInActivity(View view) {
         startActivity(new Intent(getApplicationContext(), SignInActivity.class));
     }
+
 }
