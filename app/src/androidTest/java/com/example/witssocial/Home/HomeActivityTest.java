5572aaddc762d1c5f38cdf9a3749e797ae337a5b
Katleho <1355854@students.wits.ac.za>
2022-06-01 16:21:18 +0200

    Fixing testing bug

--- app/src/androidTest/java/com/example/witssocial/Home/HomeActivityTest.java
+++ app/src/androidTest/java/com/example/witssocial/Home/HomeActivityTest.java
@@ -4,16 +4,23 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
 
 import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
 
 import com.example.witssocial.Authentication.check_mail;
 import com.example.witssocial.R;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,6 +30,20 @@ public class HomeActivityTest {
     @Rule
     public ActivityScenarioRule<HomeActivity> homeActivityActivityScenarioRule =
             new ActivityScenarioRule<HomeActivity>(HomeActivity.class);
+    @BeforeClass
+    public static void dismissANRSystemDialog() throws UiObjectNotFoundException {
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        // If the device is running in English Locale
+        UiObject waitButton = device.findObject(new UiSelector().textContains("wait"));
+        if (waitButton.exists()) {
+            waitButton.click();
+        }
+        // If the device is running in Japanese Locale
+        waitButton = device.findObject(new UiSelector().textContains("待機"));
+        if (waitButton.exists()) {
+            waitButton.click();
+        }
+    }
 
     @Test
     public void onCreate() {
