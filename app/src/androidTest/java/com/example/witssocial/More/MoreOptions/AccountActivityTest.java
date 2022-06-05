37a4b82f54b2d5102ae532fcf78852900427b092
Katleho <1355854@students.wits.ac.za>
2022-06-05 04:59:10 +0200

    Testing

--- app/src/androidTest/java/com/example/witssocial/More/MoreOptions/AccountActivityTest.java
+++ app/src/androidTest/java/com/example/witssocial/More/MoreOptions/AccountActivityTest.java
@@ -3,11 +3,14 @@ package com.example.witssocial.More.MoreOptions;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.*;
 
 import android.app.UiAutomation;
 
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -26,7 +29,7 @@ public class AccountActivityTest {
 
     @Test
     public void test_PageinView(){
-        onView(withId(R.id.profileActivity)).check(matches(isDisplayed()));
-        onView(withId(R.id.forgotPassActivity)).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.profileActivity))).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.forgotPassActivity)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 }
